72421ce9a9514d4d868483532ef446e348e22c39
Anna Korkina <90460093+kegly@users.noreply.github.com>
2022-05-04 11:25:39 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,34 +1,52 @@
-fin = open('input.txt', 'r')
-n = fin.readline()
-x = list(map(int, fin.readline().split()))
-fin.close()
-fout = open('output.txt', 'w')
-flag = 1
+def canDivision(coins, n, first, second, third, backs):
+    if (first == 0) and (second == 0) and (third == 0):
+        if (n < 0):
+            return True
+        else:
+            return False
 
-if sum(x) % 3 == 0:
-  tmp = sum(x) / 3
-  x.sort()
-  x.reverse()
-  for i in range(3):
-    y = []
-    j = 0
-    while (sum(y) != tmp and j<len(x)):
-      if ((sum(y) + x[j]) <= tmp):
-        y.append(x[j])
-        del(x[j])
-      else:
-        j += 1
-    if sum(y) == tmp:
-      pass
-      #print(y)
-    else:
-      flag  = 0
-      break
+    if (n < 0):
+        return False
+
+    way = (first, second, third, n)
+    if (way not in backs):
+        inFirst = False
+        inSecond = False
+        inThird = False
+
+        if (first - coins[n] >= 0):
+            inFirst = canDivision(coins, n - 1, first - coins[n], second, third, backs)
+        if (inFirst == False) and (second - coins[n] >= 0):
+            inSecond = canDivision(coins, n - 1, first, second - coins[n], third, backs)
+        if (inFirst == False) and (inSecond == False) and (third - coins[n] >= 0):
+            inThird = canDivision(coins, n - 1, first, second, third - coins[n], backs)
+
+        backs[way] = inFirst or inSecond or inThird
+
+    return backs[way]
+
+
+inp = open("input.txt")
+
+n = int(inp.readline())
+coins = list(map(int, inp.readline().split()))
+
+summ = sum(coins)
+if (summ % 3 != 0) or (n < 3):
+    out = open("output.txt", "w")
+    out.write("0")
+    out.close()
+    exit
+
+need = summ / 3
+
+backs = {}
+
+res = canDivision(coins, n - 1, need, need, need, backs)
+
+out = open("output.txt", "w")
+if (res):
+    out.write("1")
 else:
-  flag = 0
-
-
-if flag:
-  fout.write('1')
-else:
-  fout.write('0')
+    out.write("0")
+out.close()
